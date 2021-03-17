45a9f4c85dbfd4e718826271988b2126d4f91088
vladoja <dr.jalo2@gmail.com>
2021-03-17 11:24:43 +0100

    Click on button event

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :style="{background: color}" class="btn">{{text}}</button>
+    <button @click="onClick()" :style="{background: color}" class="btn">{{text}}</button>
 </template>
 
 
@@ -9,6 +9,11 @@ export default {
     props: {
         text: String,
         color: String
+    },
+    methods: {
+        onClick() {
+            console.log('click');
+        }
     }
 }
 </script>
