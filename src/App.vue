b55c581046a7803ea1fd8a77ba05192f82b76d06
vladoja <dr.jalo2@gmail.com>
2021-03-18 13:58:49 +0100

    T_01_39_00 vue-router with About.vue

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,14 @@
     </div>
 
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 import Tasks from "@/components/Tasks.vue";
 import AddTask from "@/components/AddTask.vue";
 
@@ -22,6 +25,7 @@ export default {
   name: "App",
   components: {
     Header,
+    Footer,
     Tasks,
     AddTask
   },
